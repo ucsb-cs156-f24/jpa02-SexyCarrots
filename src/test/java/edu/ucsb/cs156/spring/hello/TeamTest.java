5e18196282348bf998a21bc3ef3e99e2912ea0c5
Xinghan Yang <xinghan@ucsb.edu>
2024-10-14 11:43:08 -0700

    d

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,8 +19,40 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_correct() {
+        Team x = new Team("test-team");
+        Team y = new Team("2");
+        int num=10;
+        assert(team.equals(team));
+        assert(team.equals(x));
+        assert(!team.equals(y));
+        assert(!team.equals(num));
+
+    }
+    @Test
+    public void hashCode_correct(){
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+     
+    }
+    @Test
+    public void hashCode_correct1(){
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+     
+    }
 
 }
